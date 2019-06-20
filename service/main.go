f8214ee9e4c4ca2e96b945388e90a2b43e4e76dc
Joe Kratzat <joe.kratzat@gmail.com>
2019-06-19 21:51:58 -0400

    add basic tracing

--- service/main.go
+++ service/main.go
@@ -13,11 +13,21 @@ type serviceHandler struct {
 
 func (h *serviceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	id := strings.TrimPrefix(r.URL.Path, "/service/")
+	requestID := r.Header.Get("x-request-id")
 	hostname, _ := os.Hostname()
+	log.Println("x-request-id ", requestID)
 	fmt.Fprintf(w, " Hello from behind Envoy service%s Host: %s\n", id, hostname)
 }
 
 func main() {
+	f, err := os.OpenFile("service.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+			log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+
 	http.Handle("/service/", new(serviceHandler))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
